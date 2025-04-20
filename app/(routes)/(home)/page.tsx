548022c4d2b566bd04592199b8fc9093f3f4ef80
amir-9 <aghaamirjan13@gmail.com>
2025-04-20 19:17:28 +0330

    .

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Header from "@/components/utils/Header";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <div>خانه</div>
-    </>
+    <div className="container pt-[70px]">
+      <Header>خانه</Header>
+      <div>
+        <Link href={"/team"}>
+          <Button variant={"link"}>معرفی اعضا</Button>
+        </Link>
+      </div>
+    </div>
   );
 }
